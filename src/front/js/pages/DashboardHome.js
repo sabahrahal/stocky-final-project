b514c1022d06384588ae6d4eafb6b4054888817b
Jorge Oviedo <jorgeoviedo1999@gmail.com>
2022-12-06 15:19:56 +0000

    add front for edit user and edit company

--- src/front/js/pages/DashboardHome.js
+++ src/front/js/pages/DashboardHome.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/dashboard.css"
